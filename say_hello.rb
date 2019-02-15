9cac0f7b4376d90efc807ca2493065f9d9cbb8c8
Alison Riith <alisonriith@gmail.com>
2019-02-15 15:49:23 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
-def say_hello(name = "Kent Beck")
+def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
-puts say_hello
-puts say_hello("Ruby Programmer")
+puts say_hello("Kent Beck")
+puts say_hello()
